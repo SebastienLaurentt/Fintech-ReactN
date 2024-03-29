8e902d9a6a65a27c0b343efa981af3760074a438
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-29 23:57:05 +0100

    add content and login button

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -43,3 +43,4 @@ function RootLayoutNav() {
     </Stack>
   );
 }
+
--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,8 @@
 import { useAssets } from "expo-asset";
 import { Video } from "expo-av";
+import { Link } from "expo-router";
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const Page = () => {
   const [assets] = useAssets([require("@/assets/videos/intro.mp4")]);
@@ -16,12 +17,24 @@ const Page = () => {
           style={styles.video}
         />
       )}
+      <View style={{ marginTop: 80, padding: 20 }}>
+        <Text style={styles.header}>Ready to change the way you money?</Text>
+      </View>
+
+      <View style={styles.buttons}>
+        <Link href={"/login"} asChild>
+          <TouchableOpacity>
+            <Text style={{ color: "white" }}>Login In</Text>
+          </TouchableOpacity>
+        </Link>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 40,
     flex: 1,
     justifyContent: "space-between",
   },
@@ -29,6 +42,20 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
     position: "absolute",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  header: {
+    fontSize: 36,
+    fontWeight: "900",
+    textTransform: "uppercase",
+    color: "white",
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 20,
+    marginBottom: 60,
   },
 });
 
