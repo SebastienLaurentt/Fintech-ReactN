36bad184ee3c741d8dc49725beb6a6911fcad91d
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-30 15:04:42 +0100

    add content and input

--- app/signup.tsx
+++ app/signup.tsx
@@ -1,12 +1,49 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import Colors from "@/constants/Colors";
+import { defaultStyles } from "@/constants/Styles";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { TextInput } from "react-native";
 
 const signup = () => {
-  return (
-    <View>
-      <Text>signup</Text>
-    </View>
-  )
-}
+  const [countryCode, setCountryCode] = useState("+49");
+  const onSignup = async () => {};
 
-export default signup
+  return (
+    <View style={defaultStyles.container}>
+      <Text style={defaultStyles.header}>Let's get Started </Text>
+      <Text style={defaultStyles.descriptionText}>
+        {" "}
+        Enter your phone number. We will send you a confirmation code there
+      </Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Country Code"
+          placeholderTextColor={Colors.gray}
+          value={countryCode}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Mobile number"
+          keyboardType="numeric"
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginVertical: 40,
+    flexDirection: "row",
+  },
+  input: {
+    backgroundColor: Colors.lightGray,
+    padding:20,
+    borderRadius: 16,
+    fontSize:20,
+    marginRight: 10,
+  },
+});
+
+export default signup;
