bb5397dc0cd0badfb7771135962df1d2cda26de6
Sebastien Laurent <slaurent.26@gmail.com>
2024-03-31 22:27:17 +0200

    update sign in cases

--- app/login.tsx
+++ app/login.tsx
@@ -1,6 +1,6 @@
 import Colors from "@/constants/Colors";
 import { defaultStyles } from "@/constants/Styles";
-import { Link } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 import React, { useState } from "react";
 import {
   KeyboardAvoidingView,
@@ -12,17 +12,28 @@ import {
   View,
 } from "react-native";
 
+enum SignInType {
+  Phone,
+  Email,
+  Google,
+  Apple,
+}
+
 const login = () => {
   const [countryCode, setCountryCode] = useState("+49");
   const [phoneNumber, setPhoneNumber] = useState("");
-  const keyboardVerticalOffset = Platform.OS === 'ios' ? 80 : 0;
+  const keyboardVerticalOffset = Platform.OS === "ios" ? 80 : 0;
 
-  const onSignup = async () => {};
+  const onSignIn = async (type: SignInType) => {};
 
   return (
-    <KeyboardAvoidingView style={{flex:1}} behavior="padding" keyboardVerticalOffset={keyboardVerticalOffset}>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior="padding"
+      keyboardVerticalOffset={keyboardVerticalOffset}
+    >
       <View style={defaultStyles.container}>
-        <Text style={defaultStyles.header}>Let's get Started </Text>
+        <Text style={defaultStyles.header}>Welcome back</Text>
         <Text style={defaultStyles.descriptionText}>
           {" "}
           Enter your phone number. We will send you a confirmation code there
@@ -44,23 +55,75 @@ const login = () => {
           />
         </View>
 
-        <Link href={"/login"} replace asChild>
-          <Text style={defaultStyles.textLink}>
-            Already have an account? Login
-          </Text>
-        </Link>
-
-        <View style={{ flex: 1 }} />
-
         <TouchableOpacity
           style={[
             defaultStyles.pillButton,
             phoneNumber !== "" ? styles.enabled : styles.disabled,
             { marginBottom: 20 },
           ]}
-          onPress={onSignup}
+          onPress={() => onSignIn(SignInType.Phone)}
         >
-          <Text style={defaultStyles.buttonText}>Sign up</Text>
+          <Text style={defaultStyles.buttonText}>Continue</Text>
+        </TouchableOpacity>
+
+        <View style={{ flexDirection: "row", alignItems: "center", gap: 16 }}>
+          <View style={{ flex: 1, height: 2, backgroundColor: Colors.gray }} />
+          <Text style={{ color: Colors.gray, fontSize: 20 }}>or</Text>
+          <View style={{ flex: 1, height: 2, backgroundColor: Colors.gray }} />
+        </View>
+
+        <TouchableOpacity
+          onPress={() => onSignIn(SignInType.Email)}
+          style={[
+            defaultStyles.pillButton,
+            {
+              flexDirection: "row",
+              gap: 16,
+              marginTop: 20,
+              backgroundColor: "#fff",
+            },
+          ]}
+        >
+          <Ionicons name="mail" size={24} color={"#000"} />
+          <Text style={[defaultStyles.buttonText, { color: "#000" }]}>
+            Continue with email{" "}
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => onSignIn(SignInType.Google)}
+          style={[
+            defaultStyles.pillButton,
+            {
+              flexDirection: "row",
+              gap: 16,
+              marginTop: 20,
+              backgroundColor: "#fff",
+            },
+          ]}
+        >
+          <Ionicons name="logo-google" size={24} color={"#000"} />
+          <Text style={[defaultStyles.buttonText, { color: "#000" }]}>
+            Continue with email{" "}
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => onSignIn(SignInType.Apple)}
+          style={[
+            defaultStyles.pillButton,
+            {
+              flexDirection: "row",
+              gap: 16,
+              marginTop: 20,
+              backgroundColor: "#fff",
+            },
+          ]}
+        >
+          <Ionicons name="logo-apple" size={24} color={"#000"} />
+          <Text style={[defaultStyles.buttonText, { color: "#000" }]}>
+            Continue with email{" "}
+          </Text>
         </TouchableOpacity>
       </View>
     </KeyboardAvoidingView>
