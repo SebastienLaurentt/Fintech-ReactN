b18cb748735d1946dea7c433c06d810e3682ae2d
Sebastien Laurent <slaurent.26@gmail.com>
2024-04-01 16:35:53 +0200

    add help and verify stack.screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,14 +4,13 @@ import { Ionicons } from "@expo/vector-icons";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { useFonts } from "expo-font";
 import { Link, Stack, useRouter, useSegments } from "expo-router";
+import * as SecureStore from "expo-secure-store";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 import { Text, TouchableOpacity } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
-import * as SecureStore from 'expo-secure-store';
-
 
 // Cache the Clerk JWT
 const tokenCache = {
@@ -33,7 +32,7 @@ const tokenCache = {
 
 export {
   // Catch any errors thrown by the Layout component.
-  ErrorBoundary
+  ErrorBoundary,
 } from "expo-router";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -61,20 +60,20 @@ const InitialLayout = () => {
   }, [loaded]);
 
   useEffect(() => {
-    console.log('isSignedIn', isSignedIn);
+    console.log("isSignedIn", isSignedIn);
     if (!isLoaded) return;
 
-    const inAuthGroup = segments[0] === '(authenticated)';
+    const inAuthGroup = segments[0] === "(authenticated)";
 
     if (isSignedIn && !inAuthGroup) {
-      router.replace('/(authenticated)/(tabs)/home');
+      router.replace("/(authenticated)/(tabs)/home");
     } else if (!isSignedIn) {
-      router.replace('/');
+      router.replace("/");
     }
   }, [isSignedIn]);
 
   if (!loaded || !isLoaded) {
-    return <Text> Loading ... </Text>
+    return <Text> Loading ... </Text>;
   }
 
   return (
@@ -94,12 +93,12 @@ const InitialLayout = () => {
           ),
         }}
       />
-      
+
       <Stack.Screen
         name="login"
         options={{
-          title: '',
-          headerBackTitle: '',
+          title: "",
+          headerBackTitle: "",
           headerShadowVisible: false,
           headerStyle: { backgroundColor: Colors.background },
           headerLeft: () => (
@@ -108,21 +107,49 @@ const InitialLayout = () => {
             </TouchableOpacity>
           ),
           headerRight: () => (
-            <Link href={'/help'} asChild>
+            <Link href={"/help"} asChild>
               <TouchableOpacity>
-                <Ionicons name="help-circle-outline" size={34} color={Colors.dark} />
+                <Ionicons
+                  name="help-circle-outline"
+                  size={34}
+                  color={Colors.dark}
+                />
               </TouchableOpacity>
             </Link>
           ),
         }}
       />
+
+      <Stack.Screen
+        name="help"
+        options={{ title: "Help", presentation: "modal" }}
+      />
+
+      <Stack.Screen
+        name="verify/[phone]"
+        options={{
+          title: "",
+          headerBackTitle: "",
+          headerShadowVisible: false,
+          headerStyle: { backgroundColor: Colors.background },
+          headerLeft: () => (
+            <TouchableOpacity onPress={router.back}>
+              <Ionicons name="arrow-back" size={34} color={Colors.dark} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      
     </Stack>
   );
 };
 
 const RootLayoutNav = () => {
   return (
-    <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY!} tokenCache={tokenCache}>
+    <ClerkProvider
+      publishableKey={CLERK_PUBLISHABLE_KEY!}
+      tokenCache={tokenCache}
+    >
       <GestureHandlerRootView style={{ flex: 1 }}>
         <StatusBar style="light" />
         <InitialLayout />
