7937a26341d225f9edab8a35246d9cf4c4b3f9e5
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-02 11:39:19 +0200

    add content in home tabs

--- app/(authenticated)/(tabs)/home.tsx
+++ app/(authenticated)/(tabs)/home.tsx
@@ -1,11 +1,64 @@
-import { Text, View } from "react-native";
+import Colors from "@/constants/Colors";
+import { Button, ScrollView, StyleSheet, Text, View } from "react-native";
 
 const Page = () => {
+  const balance = 1420;
   return (
-    <View>
-      <Text> Layout </Text>
-    </View>
+    <ScrollView style={{ backgroundColor: Colors.background }}>
+      <View style={styles.account}>
+        <View style={styles.row}>
+          <Text style={styles.balance}>{balance}</Text>
+          <Text style={styles.currency}>$</Text>
+        </View>
+      </View>
+
+      <View style={styles.actionRow}>
+        <Button title="send" />
+        <Button title="Request" />
+      </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  account: {
+    margin: 80,
+    alignItems: "center",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "flex-end",
+    justifyContent: "center",
+    gap: 10,
+  },
+  balance: {
+    fontSize: 50,
+    fontWeight: "bold",
+  },
+  currency: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  actionRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 20,
+  },
+  transactions: {
+    marginHorizontal: 20,
+    padding: 14,
+    backgroundColor: "#fff",
+    borderRadius: 16,
+    gap: 20,
+  },
+  circle: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: Colors.lightGray,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default Page;
