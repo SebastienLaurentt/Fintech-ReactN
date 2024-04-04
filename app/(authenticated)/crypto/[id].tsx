68924d6d63bfd897c9e8207c4d3fcd23a99932e5
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-04 17:17:01 +0200

    update crypto page id with section list (wip)

--- app/(authenticated)/crypto/[id].tsx
+++ app/(authenticated)/crypto/[id].tsx
@@ -1,15 +1,69 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import Colors from "@/constants/Colors";
+import { defaultStyles } from "@/constants/Styles";
+import { useHeaderHeight } from "@react-navigation/elements";
+import { Stack, useLocalSearchParams } from "expo-router";
+import React from "react";
+import { SectionList, StyleSheet, Text, View } from "react-native";
 
 const Page = () => {
-
-  const { id } = useLocalSearchParams()
+  const { id } = useLocalSearchParams();
+  const headerHeight = useHeaderHeight();
   return (
-    <View>
-      <Text>Page</Text>
-    </View>
-  )
-}
+    <>
+      <Stack.Screen options={{ title: "DETAILS" }} />
+      <SectionList
+        style={{ paddingTop: headerHeight }}
+        keyExtractor={(i) => i.title}
+        sections={[{ data: [{ title: "Chart" }] }]}
+        renderItem={({ item }) => (
+          <>
+            <View style={[defaultStyles.block, { marginTop: 20 }]}>
+              <Text style={styles.subtitle}>Overview</Text>
+              <Text style={{ color: Colors.gray }}>
+                Bitcoin is a decentralized digital currency, without a central
+                bank or single administrator, that can be sent from user to user
+                on the peer-to-peer bitcoin network without the need for
+                intermediaries.
+              </Text>
+            </View>
+          </>
+        )}
+      ></SectionList>
+    </>
+  );
+};
 
-export default Page
+const styles = StyleSheet.create({
+  subtitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 20,
+    color: Colors.gray,
+  },
+  categoryText: {
+    fontSize: 14,
+    color: Colors.gray,
+  },
+  categoryTextActive: {
+    fontSize: 14,
+    color: "#000",
+  },
+  categoriesBtn: {
+    padding: 10,
+    paddingHorizontal: 14,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 20,
+  },
+  categoriesBtnActive: {
+    padding: 10,
+    paddingHorizontal: 14,
+
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    borderRadius: 20,
+  },
+});
+
+export default Page;
