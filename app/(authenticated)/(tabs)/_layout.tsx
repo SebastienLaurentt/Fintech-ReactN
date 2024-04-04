efc25b15fade70dc1163a25717e0bcf4c75b9faf
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-04 14:03:34 +0200

    import customHeader in home Tabs.screen

--- app/(authenticated)/(tabs)/_layout.tsx
+++ app/(authenticated)/(tabs)/_layout.tsx
@@ -1,3 +1,4 @@
+import CustomHeader from "@/components/CustomHeader";
 import Colors from "@/constants/Colors";
 import { FontAwesome } from "@expo/vector-icons";
 import { BlurView } from "expo-blur";
@@ -11,23 +12,24 @@ const Layout = () => {
         tabBarBackground: () => (
           <BlurView
             intensity={100}
-            tint={'extraLight'}
+            tint={"extraLight"}
             style={{
               flex: 1,
-              backgroundColor: 'rgba(0,0,0,0.05)',
+              backgroundColor: "rgba(0,0,0,0.05)",
             }}
           />
         ),
         tabBarStyle: {
-          backgroundColor: 'transparent',
-          position: 'absolute',
+          backgroundColor: "transparent",
+          position: "absolute",
           bottom: 0,
           left: 0,
           right: 0,
           elevation: 0,
           borderTopWidth: 0,
         },
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="home"
         options={{
@@ -35,6 +37,8 @@ const Layout = () => {
           tabBarIcon: ({ size, color }) => (
             <FontAwesome name="registered" size={size} color={color} />
           ),
+          header: () => <CustomHeader />,
+          headerTransparent: true,
         }}
       />
       <Tabs.Screen
